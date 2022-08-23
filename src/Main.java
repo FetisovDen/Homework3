5702457032e9a312a90fb5ba1b92a6c06b37d6f7
FetisovDen <fetisovden@yandex,ru>
2022-08-23 15:38:58 +0300

    Homework3 сделано

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//Задание 1
+int clientOS = 1;
+if (clientOS == 0)
+{ System.out.println("Установите версию приложения для iOS по ссылке");}
+if (clientOS == 1)
+{ System.out.println("Установите версию приложения для Android по ссылке");}
+
+//Задание 2
+
+int clientOs = 1;
+int clientDeviceYear = 2014;
+
+if (clientOs == 0 && clientDeviceYear < 2015)
+{ System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+if (clientOs == 1 && clientDeviceYear < 2015)
+{ System.out.println("Установите облегченную версию приложения для Android по ссылке");}
+
+if (clientOs == 0 && clientDeviceYear >= 2015)
+{ System.out.println("Установите версию приложения для iOS по ссылке");}
+if (clientOs == 1 && clientDeviceYear >= 2015)
+{ System.out.println("Установите версию приложения для Android по ссылке");}
+
+//Задание 3
+
+double year = 2021;
+if ((year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0 ) {
+System.out.println("год является високосным");}
+else {System.out.println("год не является високосным");}
+
+//Задание 4
+
+int deliveryDistance = 95;
+if(deliveryDistance <= 20) {
+    System.out.println( "Потребуется дней: " + (1+((deliveryDistance - 20) / 40)));}
+if(deliveryDistance > 20) {
+    System.out.println( "Потребуется дней: " + (2+((deliveryDistance - 20) / 40)));}
+
+//Задание 5
+
+int monthNumber = 6;
+switch (monthNumber) {
+case 1:
+case 2:
+case 12:
+System.out.println("Зима");
+break;
+case 3:
+case 4:
+case 5:
+System.out.println("Весна");
+break;
+case 6:
+case 7:
+case 8:
+System.out.println("Лето");
+break;
+case 9:
+case 10:
+case 11:
+System.out.println("Осень");
+break;
+default:
+System.out.println("Такого месяца не существует");
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
